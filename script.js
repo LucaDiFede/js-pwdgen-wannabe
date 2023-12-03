cc1f3cd97ccf659ab0aee2f962f23f1f4f65e22b
LucaDiFede <146111756+LucaDiFede@users.noreply.github.com>
2023-12-03 12:24:05 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const Nome = prompt("Nome");
+console.log('Nome:', Nome, typeof Nome);
+
+const Cognome = prompt("Cognome");
+console.log('Cognome:', Cognome, typeof Cognome);
+
+const Colore = prompt("Colore preferito");
+console.log('Colore:', Colore, typeof Colore);
+
+document.writeln('nomecognomecolorepreferito114');
